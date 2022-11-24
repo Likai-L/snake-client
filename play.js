7b3f2673ae25d6d2957868042e29362ad2e8b80c
Likai Liu <likailiu2@gmail.com>
2022-11-24 07:10:57 +0000

    Modulize the user input functions

--- play.js
+++ play.js
@@ -1,21 +1,8 @@
-const net = require("net");
-
 const { connect } = require("./client");
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+const { setupInput } = require("./input");
 
-const handleUserInput = (key) => {
-  if (key === "\u0003") {
-    process.exit();
-  }
-};
-setupInput();
 console.log("Connecting ...");
 connect();
+
+setupInput();
